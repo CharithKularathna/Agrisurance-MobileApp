2974a70809642931ca7439497ac7309d8bf0548e
Charith Kularathna <charithkularathna.17@cse.mrt.ac.lk>
2021-12-07 18:50:56 +0530

    thunk added, dev tools working

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { rootReducer } from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
 
 
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+))
